40d7904f26834756f71b3aeaae2872f956f99843
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-25 18:10:44 +0530

    mongoose connection successfully

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Something went wrong!";
+  return res.status(errorStatus).send(errorMessage);
+});
+
+app.listen(8800, async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB!");
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("Backend server is running!");
+});
