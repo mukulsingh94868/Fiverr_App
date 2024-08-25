9178d8d4fddb7f604436bcde7a13fcf6272131d7
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-25 18:23:25 +0530

    make all routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,8 @@ mongoose.set("strictQuery", true);
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+
+app.use("/api/user", userRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
