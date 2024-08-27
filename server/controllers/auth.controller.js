c01a09e59aa49d480454b0f4ec67e8ddbd942d5f
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-27 15:44:34 +0530

    login api backend

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -20,14 +20,39 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) return res.status(404).send("User not found!");
+
+    const isCorrect = bcrypt.compareSync(req.body.password, user.password);
+    if (!isCorrect)
+      return res.status(404).send("Wrong password or username!");
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+        isSeller: user.isSeller,
+      },
+      process.env.JWT_KEY
+    );
+
+    const { password, ...info } = user._doc;
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
   } catch (err) {
-    next(err);
+    // next(err);
+    console.log('err', err);
   }
 };
 
 export const logout = async (req, res, next) => {
   try {
   } catch (err) {
-    next(err);
+    // next(err);
+    console.log('err', err);
   }
 };
