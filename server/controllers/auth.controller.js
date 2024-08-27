6afb50c7ff93a564707f6135b5ef7e3e5a3e4b52
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-08-27 16:12:08 +0530

    delete and get user by id of users

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -13,20 +14,18 @@ export const register = async (req, res, next) => {
     await newUser.save();
     res.status(201).send("User has been created.");
   } catch (err) {
-    console.log('err', err);
-    // next(err);
+    next(err);
   }
 };
-
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
 
-    if (!user) return res.status(404).send("User not found!");
+    if (!user) return next(createError(404, "User not found!"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
     if (!isCorrect)
-      return res.status(404).send("Wrong password or username!");
+      return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
       {
@@ -44,15 +43,16 @@ export const login = async (req, res, next) => {
       .status(200)
       .send(info);
   } catch (err) {
-    // next(err);
-    console.log('err', err);
+    next(err);
   }
 };
 
-export const logout = async (req, res, next) => {
-  try {
-  } catch (err) {
-    // next(err);
-    console.log('err', err);
-  }
+export const logout = async (req, res) => {
+  res
+    .clearCookie("accessToken", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
 };
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import { deleteUser, getUser } from "../controllers/user.controller.js";
+import { verifyToken } from "../middleware/jwt.js";
 
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-    res.send("Hello World");
-})
+router.delete("/:id", verifyToken, deleteUser);
+router.get("/:id", getUser);
 
 export default router;
