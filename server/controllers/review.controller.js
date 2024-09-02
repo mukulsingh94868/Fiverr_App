83a563f0aa40643a38f2cab0413cd532a0337b51
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-09-02 18:02:39 +0530

    review and api review

---
+++ server/controllers/review.controller.js
@@ -0,0 +1,54 @@
+import Review from '../models/review.model.js';
+import createError from '../utils/createError';
+
+export const createReview = async (req, res) => {
+    if (req.isSeller)
+        return next(createError(403, "Sellers can't create a review!"));
+
+    const newReview = new Review({
+        userId: req.userId,
+        gigId: req.body.gigId,
+        desc: req.body.desc,
+        star: req.body.star,
+    });
+
+    try {
+        const review = await Review.findOne({
+            gigId: req.body.gigId,
+            userId: req.userId,
+        });
+
+        if (review)
+            return next(
+                createError(403, "You have already created a review for this gig!")
+            );
+
+        //TODO: check if the user purchased the gig.
+
+        const savedReview = await newReview.save();
+
+        await Gig.findByIdAndUpdate(req.body.gigId, {
+            $inc: { totalStars: req.body.star, starNumber: 1 },
+        });
+        res.status(201).send(savedReview);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const getReviews = async (req, res) => {
+    try {
+        const reviews = await Review.find({ gigId: req.params.gigId });
+        res.status(200).send(reviews);
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const deleteReview = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.log('error', error);
+    }
+};
