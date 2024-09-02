c06e1276abfc181a287d90ec6196715ba6f27a14
mukulsingh94868 <mukulsingh94868@gmail.com>
2024-09-02 14:01:10 +0530

    indentation

--- server/controllers/gig.controller.js
+++ server/controllers/gig.controller.js
@@ -2,42 +2,43 @@ import createError from "../utils/createError.js";
 import Gig from "../models/gig.model.js";
 
 export const createGig = async (req, res, next) => {
-    if (!req.isSeller)
-      return next(createError(403, "Only sellers can create a gig!"));
-  
-    const newGig = new Gig({
-      userId: req.userId,
-      ...req.body,
-    });
-  
-    try {
-      const savedGig = await newGig.save();
-      res.status(201).json(savedGig);
-    } catch (err) {
-      next(err);
-    }
-  };
+  if (!req.isSeller) {
+    return next(createError(403, "Only sellers can create a gig!"));
+  }
+
+  const newGig = new Gig({
+    userId: req.userId,
+    ...req.body,
+  });
+
+  try {
+    const savedGig = await newGig.save();
+    res.status(201).json(savedGig);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const deleteGig = async (req, res, next) => {
-    try {
+  try {
 
-    } catch (error) {
-        console.log('error', error);
-    }
+  } catch (error) {
+    console.log('error', error);
+  }
 };
 
 export const getGig = async (req, res, next) => {
-    try {
+  try {
 
-    } catch (error) {
-        console.log('error', error);
-    }
+  } catch (error) {
+    console.log('error', error);
+  }
 };
 
 export const getGigs = async (req, res, next) => {
-    try {
+  try {
 
-    } catch (error) {
-        console.log('error', error);
-    }
+  } catch (error) {
+    console.log('error', error);
+  }
 };
